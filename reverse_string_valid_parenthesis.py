4e91fe625fb280414367cc95817850a29e22d576
Raji <chidambaram.spv@gmail.com>
2018-02-14 15:41:43 -0800

    reverse_string/valid_parenthesis

---
+++ reverse_string_valid_parenthesis.py
@@ -0,0 +1,44 @@
+#Buddy Strings
+#Given two strings A and B of lowercase letters, return true if and only if we can swap two letters in A so that the result equals B.
+0 <= A.length <= 20000
+0 <= B.length <= 20000
+A and B consist only of lowercase letters.
+
+#Here I have considered only the last two elements, meaning only the last two characters in A are reversed (along with the rest of A up until the last two elements)
+#and verified whether they are equivalent to B
+
+class Solution:
+    def buddyStrings(self, A, B):
+        if A=="" or B=="":
+            return False
+        elif len(A) != len(B):
+            return False
+        elif (len(A)>=2 and len(A)<= 20000) and (len(B)>=2 and len(B)<= 20000):
+            res=''.join((A[:-2], A[-2:][::-1]))
+            if res==B:
+                return True
+            else:
+                return False
+
+#For reference from career cup: Given "n", generate all valid parenthesis strings of length "2n". 
+Example: 
+Given n=2 
+
+Output: 
+(()) 
+()()
+
+def paren(left,right,string):
+    if(left == 0 and right == 0):
+        print (string)
+    if(left>right):
+        return
+    if (left > 0):
+        paren(left-1,right,string+"(")
+    if (right > 0):
+        paren(left,right-1,string+")")
+
+def parentheses(n):
+    paren(n/2,n/2,"")
+
+
