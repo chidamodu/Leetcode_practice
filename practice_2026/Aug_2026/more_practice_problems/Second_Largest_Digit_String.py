a2a8f2a73c42ba0c97ee441eb623842a4895ed62
Chidaod_foc$$ <chidambaram.spv@gmail.com>
2026-08-19 22:02:56 -0700

    Second Largest Digit in a String
    Second Largest Digit in a String
    Given an alphanumeric string s, return the second largest numerical digit that appears in s, or -1 if it does not exist.
    An alphanumeric string is a string consisting of lowercase English letters and digits.
    
    Example 1:
    Input: s = "dfa12321afd"
    Output: 2
    Explanation: The digits that appear in s are [1, 2, 3]. The second largest digit is 2.
    
    Example 2:
    Input: s = "abc1111"
    Output: -1
    Explanation: The digits that appear in s are [1]. There is no second largest digit. 
     
    Constraints:
    1 <= s.length <= 500
    s consists of only lowercase English letters and digits.

---
+++ practice_2026/Aug_2026/more_practice_problems/Second_Largest_Digit_String.py
@@ -0,0 +1,34 @@
+# Second Largest Digit in a String
+# Given an alphanumeric string s, return the second largest numerical digit that appears in s, or -1 if it does not exist.
+# An alphanumeric string is a string consisting of lowercase English letters and digits.
+
+# Example 1:
+# Input: s = "dfa12321afd"
+# Output: 2
+# Explanation: The digits that appear in s are [1, 2, 3]. The second largest digit is 2.
+
+# Example 2:
+# Input: s = "abc1111"
+# Output: -1
+# Explanation: The digits that appear in s are [1]. There is no second largest digit. 
+ 
+# Constraints:
+# 1 <= s.length <= 500
+# s consists of only lowercase English letters and digits.
+
+def secondHighest(s):
+    largest = -1
+    second = -1
+    for i in s:
+        if i.isdigit():
+            d = int(i)
+            if d > largest:
+                second = largest
+                largest = d
+            elif d > second and d != largest:
+                second = d
+    return print(second)
+
+secondHighest("abc1111")
+# "dfa12321afd"
+ 
